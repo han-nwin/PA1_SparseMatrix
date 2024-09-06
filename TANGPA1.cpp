dad3cc3318af2d4f7f815ceadf9ffffd68519f8c
han-nwin <hannguyen.win@gmail.com>
2024-09-06 17:42:46 -0500

    update

--- TANGPA1.cpp
+++ TANGPA1.cpp
@@ -21,8 +21,8 @@ struct Node {
     int data;     ///< The data value stored in the node.
     int rowIndex; ///< The row index of the node (for the matrix).
     int colIndex; ///< The column index of the node (for the matrix).
-    Node* rowPtr; ///< Pointer to the next node in the same row.
-    Node* colPtr; ///< Pointer to the next node in the same column.
+    Node* nextRow; ///< Pointer to the next node in the same row.
+    Node* nextCol; ///< Pointer to the next node in the same column.
 
     /**
      * @brief Constructs a new Node object.
@@ -34,8 +34,8 @@ struct Node {
      * @param rowIndex The row number of the node in the matrix.
      * @param colIndex The column number of the node in the matrix.
      */
-    Node(int data = 0, int rowIndex, int colIndex)
-        : data{data}, rowIndex{rowIndex}, colIndex{colIndex}, rowPtr{nullptr}, colPtr{nullptr} {}
+    Node(int data = 0, int rowIndex = 0, int colIndex = 0)
+        : data{data}, rowIndex{rowIndex}, colIndex{colIndex}, nextRow{nullptr}, nextCol{nullptr} {}
 };
 
 
@@ -50,7 +50,12 @@ struct Node {
  */
 class SparseMatrix {
     private:
-    Node* header = (0, 0, 0);  ///< Pointer to the head node of the circularly-linked list.
+    Node header;  ///< Pointer to the head node of the circularly-linked list.
+    int numRow;        // Number of rows in the matrix
+    int numCol;        // Number of columns in the matrix
+    Node** rowHeaders;  // Dynamic Array of pointers that point to row header nodes
+    Node** colHeaders;  // Dynamic Array of pointers that point to column header nodes
+
 
     public:
     /**
@@ -58,12 +63,73 @@ class SparseMatrix {
      *
      * Initializes a sparse matrix of the specified dimension (row x col).
      *
-     * @param rowNum The number of rows of the matrix
-     * @param colNum The number of columns of the matrix
+     * @param numRow The number of rows of the matrix
+     * @param numCol The number of columns of the matrix
      */
-    SparseMatrix(int rowNum = 0, int colNum = 0) {
-        header = new Node(0, rowNum, colNum); //Create a placeholder node at row 0, column 0 with row and col numbers.
+    SparseMatrix(int numRow = 0, int numCol = 0) : header(0, numRow, numCol), numRow(numRow), numCol(numCol) {//Create a placeholder node at 0:0 with row and col numbers stored inside rowIndex, colIndex
+        this->numRow = numRow;
+        this->numCol = numCol;
     }
+
+    /**
+     * @brief Destructor for the SparseMatrix class.
+     *
+     * The destructor is responsible for freeing all dynamically allocated memory
+     * associated with the sparse matrix. It traverses the matrix to delete
+     * all nodes and ensures that no memory leaks occur when the SparseMatrix object
+     * is destroyed.
+     */
+    ~SparseMatrix() {
+        // Delete all the row headers and their corresponding nodes.
+        for (int i = 0; i <= numRow; i++) {
+            Node* current = rowHeaders[i];
+            while (current != nullptr) {
+                Node* nextNode = current->nextRow;
+                delete current;
+                current = nextNode;
+            }
+        }
+
+        // Free the arrays of row and column headers
+        delete[] rowHeaders;
+        delete[] colHeaders;
+    }
+
+    /**
+     * @brief Returns the row length of the Sparse Matrix.
+     *
+     * This method returns the row length of the matrix. For example,
+     * if the matrix is 10x5, this method will return 10.
+     *
+     * @return int: The row length of the matrix.
+     */
+    int rowLength();
+
+    /**
+     * @brief Returns the column length of the Sparse Matrix.
+     *
+     * This method returns the column length of the matrix. For example,
+     * if the matrix is 10x5, this method will return 5.
+     *
+     * @return int: The column length of the matrix.
+     */
+    int colLength();
+
+    /**
+     * @brief Accesses the value at the specified row and column in the sparse matrix.
+     *
+     * This method retrieves the value stored in the matrix at the given row and column.
+     * If no value is stored at that position (i.e., the element is 0 in the sparse matrix),
+     * it will either return 0 or throw an exception, depending on the implementation.
+     *
+     * @param rowIndex The row index of the element to access.
+     * @param colIndex The column index of the element to access.
+     * 
+     * @return int The value at the specified row and column. Returns 0 if the element is not stored.
+     * 
+     * @throws std::out_of_range if the rowIndex or colIndex is outside the matrix bounds.
+     */
+    int access(int rowIndex, int colIndex);
 
     /**
      * @brief Adds a non-zero element to the matrix.
@@ -94,50 +160,47 @@ class SparseMatrix {
      */
     void remove(int rowIndex, int colIndex);
 
-    /**
-     * @brief Returns the row length of the Sparse Matrix.
-     *
-     * This method returns the row length of the matrix. For example,
-     * if the matrix is 10x5, this method will return 10.
-     *
-     * @return int: The row length of the matrix.
-     */
-    int rowLength();
-
-    /**
-     * @brief Returns the column length of the Sparse Matrix.
-     *
-     * This method returns the column length of the matrix. For example,
-     * if the matrix is 10x5, this method will return 5.
-     *
-     * @return int: The column length of the matrix.
-     */
-    int colLength();
-
-
 };
 
-//Implementation of add method
+//Implementation of rowLength method
+int SparseMatrix::rowLength() {
+    return this->numRow;
+}
+
+//Implementation of colLength method
+int SparseMatrix::colLength() {
+    return this->numCol;
+}
+
+//Implementation of access method
+
+
+//Implementation of insert method
 void SparseMatrix::insert(int data, int rowIndex, int colIndex) {
     // Check for out-of-bounds indices
-    if (rowIndex < 0 || rowIndex >= this->header->rowIndex || colIndex < 0 || colIndex >= this->header->colIndex) {
+    if (rowIndex <= 0 || rowIndex > this->numRow || colIndex <= 0 || colIndex > this->numCol) {
         throw std::out_of_range("Row or column index is out of bounds");
     }
-    
+     //No need to add zero value
+    if (data = 0) {
+        return;
+    }
 
+
+    
 }
 
 //Implementation of remove method
 
-//Implementation of size method
-int SparseMatrix::rowLength(){
-    return this->header->rowIndex;
-}
+
 
 
 
 
 int main(int argc, char* argv[]){
     std::cout << "This is a main program" << std::endl;
+    SparseMatrix m(5,5);
+    std::cout << "row " << m.rowLength() <<std::endl;
+    std::cout << "col " << m.colLength() <<std::endl;
     return 0;
 }
